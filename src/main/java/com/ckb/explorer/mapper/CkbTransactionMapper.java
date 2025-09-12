6e3b33d70d8a1fefcb82d60ba38a722202012549
liangyaqian <liangyaqian@rivtower.com>
2025-09-12 14:33:20 +0800

    feat: 地址的交易列表调整从24小时历史交易表里查

--- src/main/java/com/ckb/explorer/mapper/CkbTransactionMapper.java
+++ src/main/java/com/ckb/explorer/mapper/CkbTransactionMapper.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ckb.explorer.domain.dto.TransactionDto;
 import com.ckb.explorer.domain.resp.AddressTransactionPageResponse;
 import com.ckb.explorer.entity.CkbTransaction;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -16,4 +17,6 @@ public interface CkbTransactionMapper extends BaseMapper<CkbTransaction> {
   Page<AddressTransactionPageResponse> selectPageByAddressScriptId(Page page, @Param("orderByStr") String orderBy, @Param("ascOrDesc") String ascOrDesc, @Param("addressScriptId") Long addressScriptId);
 
   Page<CkbTransaction> selectPageByBlockHash(Page page, @Param("blockHash") byte[] blockHash, @Param("txHash") byte[] txHash, @Param("lockScriptId") Long lockScriptId);
+
+  List<AddressTransactionPageResponse> selectByTransactionIds( @Param("transactionIds") List<Long> transactionIds, @Param("orderByStr") String orderBy, @Param("ascOrDesc") String ascOrDesc);
 }
