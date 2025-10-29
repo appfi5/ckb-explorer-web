a4b86634176b24c029c0296d49776973ab8550f9
zhutao@rivtower.com <zhutao@rivtower.com>
2025-10-29 18:28:50 +0800

    fixUdtForNotDependTypeScriptId

--- src/main/java/com/ckb/explorer/service/impl/UdtHolderAllocationsServiceImpl.java
+++ src/main/java/com/ckb/explorer/service/impl/UdtHolderAllocationsServiceImpl.java
@@ -23,6 +23,7 @@ import com.ckb.explorer.mapper.UdtHolderAllocationsMapper;
 import com.ckb.explorer.util.I18n;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.nervos.ckb.utils.Numeric;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
@@ -30,6 +31,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * @author dell
@@ -76,25 +78,32 @@ public class UdtHolderAllocationsServiceImpl extends ServiceImpl<UdtHolderAlloca
     @Override
     public List<UdtsListResponse> udtListStatistic() {
         List<ScriptConfig.TypeScript> typeScripts = lockScriptConfig.getTypeScripts().stream()
-                .filter(typeScript -> typeScript.getCellType()!=null&&CellType.valueOf(typeScript.getCellType()).isUdtType()).toList();
-        List<Long> typeScriptIds = typeScripts.stream().map(typeScript -> {
-            return typeScript.getTypeScriptId();
-        }).toList();
+                .filter(typeScript -> typeScript.getCellType() != null && CellType.valueOf(typeScript.getCellType()).isUdtType()).toList();
+
         List<UdtsListResponse> udtsListResponses = new ArrayList<>(typeScripts.size());
 
-        // 当前数据库数量比配置少，如果后续数量多需要分页,按照udt账户数量进行分页，不再读取配置
         List<UdtAddressCountDto> addressesCounts = super.baseMapper.getAddressNum();
-        Long oneDayAgo = System.currentTimeMillis() - TimeUnit.HOURS.toMillis(24);
+
+        List<Long> typeScriptIds = addressesCounts.stream().map(UdtAddressCountDto::getTypeScriptId).collect(Collectors.toList());
+        List<Script> scripts = scriptService.listByIds(typeScriptIds);
         List<UdtH24TransactionsCountDto> transactionsCounts = address24hTransactionMapper.getTransactionsCountByScriptIds(typeScriptIds);
 
         typeScripts.stream().forEach(typeScript -> {
-            Long typeScriptId = typeScript.getTypeScriptId();
+            Long typeScriptId;
+            Script scriptData = scripts.stream().filter(script -> Objects.equals(typeScript.getScriptHash(), Numeric.toHexString(script.getScriptHash())))
+                    .findFirst().orElse(null);
+            if (scriptData != null) {
+                typeScriptId = scriptData.getId();
+            } else {
+                typeScriptId = null;
+            }
+
             UdtsListResponse udtsListResponse = new UdtsListResponse();
             udtsListResponse.setTypeScriptHash(typeScript.getScriptHash());
-            UdtAddressCountDto udtAddressCountDto = addressesCounts.stream().filter(addressesCount -> Objects.equals(addressesCount.getTypeScriptId(),typeScriptId)).findFirst().orElse(null);
+            UdtAddressCountDto udtAddressCountDto = addressesCounts.stream().filter(addressesCount -> Objects.equals(addressesCount.getTypeScriptId(), typeScriptId)).findFirst().orElse(null);
             Long addressCount = udtAddressCountDto == null ? 0L : udtAddressCountDto.getAddressesCount();
             udtsListResponse.setAddressesCount(addressCount);
-            UdtH24TransactionsCountDto udtH24TransactionsCountDto = transactionsCounts.stream().filter(transactionsCount ->Objects.equals(transactionsCount.getTypeScriptId(),typeScriptId)).findFirst().orElse(null);
+            UdtH24TransactionsCountDto udtH24TransactionsCountDto = transactionsCounts.stream().filter(transactionsCount -> Objects.equals(transactionsCount.getTypeScriptId(), typeScriptId)).findFirst().orElse(null);
             Long h24CkbTransactionsCount = udtH24TransactionsCountDto == null ? 0L : udtH24TransactionsCountDto.getH24CkbTransactionsCount();
             udtsListResponse.setH24CkbTransactionsCount(h24CkbTransactionsCount);
             udtsListResponses.add(udtsListResponse);
@@ -103,19 +112,19 @@ public class UdtHolderAllocationsServiceImpl extends ServiceImpl<UdtHolderAlloca
     }
 
     @Override
-    public UdtDetailResponse findDetailByTypeHash(String typeHash){
+    public UdtDetailResponse findDetailByTypeHash(String typeHash) {
         Script script = scriptService.findByScriptHash(typeHash);
-        if(script==null){
+        if (script == null) {
             throw new ServerException(I18nKey.UDTS_NOT_FOUND_CODE, i18n.getMessage(I18nKey.UDTS_NOT_FOUND_MESSAGE));
         }
         Long holdersCount = super.baseMapper.selectHolderCountByTypeScriptId(script.getId());
-        holdersCount = holdersCount==null?0:holdersCount;
+        holdersCount = holdersCount == null ? 0 : holdersCount;
         UdtDetailResponse udtDetailResponse = new UdtDetailResponse();
         udtDetailResponse.setHoldersCount(holdersCount);
         BigInteger totalAmount = udtAccountsMapper.getTotalAmountByTypeScriptId(script.getId());
-        totalAmount = totalAmount==null?BigInteger.ZERO:totalAmount;
+        totalAmount = totalAmount == null ? BigInteger.ZERO : totalAmount;
         udtDetailResponse.setTotalAmount(totalAmount);
-        return  udtDetailResponse;
+        return udtDetailResponse;
     }
 }
 
